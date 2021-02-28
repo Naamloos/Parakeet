54cce6a22eaf8806591750e5d5ce6e1f986f533a
Ryan <naamloosdt@gmail.com>
2021-03-01 00:51:20 +0100

    Created project and some classes

---
+++ src/Parakeet/Attributes/PacketAttribute.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Parakeet.Attributes
+{
+    // Up for consideration: automatically generate packet IDs based on eg alfabetical name (type.tostring?)
+    public class PacketAttribute : Attribute
+    {
+        public int Id { get; private set; }
+
+        public PacketAttribute(int id)
+        {
+            Id = id;
+        }
+    }
+}
---
+++ src/Parakeet/BaseServer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace Parakeet
+{
+    public abstract class BaseServer
+    {
+        public BaseServer()
+        {
+            //tweekietweekie im a parakeet
+        }
+    }
+}
